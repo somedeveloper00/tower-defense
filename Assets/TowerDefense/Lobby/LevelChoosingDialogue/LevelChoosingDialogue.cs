1bf362ad330648951dd75aa12ada31fd9df9914a
Saeed Barari <barari.saeed.am@outlook.com>
2023-05-02 10:23:28 +0330

    level choosing dialogue improved

--- Assets/TowerDefense/Lobby/LevelChoosingDialogue/LevelChoosingDialogue.cs
+++ Assets/TowerDefense/Lobby/LevelChoosingDialogue/LevelChoosingDialogue.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using AnimFlex.Sequencer.UserEnd;
 using DialogueSystem;
 using TowerDefense.Data;
 using UnityEngine;
@@ -7,14 +8,23 @@ using UnityEngine.UI;
 namespace TowerDefense.Lobby.LevelChoosing {
     public class LevelChoosingDialogue : Dialogue {
         [SerializeField] LevelStartButton levelStartButtonInstance;
+        [SerializeField] Button backgroundBtn;
         [SerializeField] Button backBtn;
+        [SerializeField] SequenceAnim outSeq;
 
         List<LevelStartButton> _levelViews = new();
 
         protected override void Start() {
             base.Start();
             setupLevelViews();
-            backBtn.onClick.AddListener( Close );
+            backBtn.onClick.AddListener( CloseWithAnim );
+            backgroundBtn.onClick.AddListener( CloseWithAnim );
+        }
+
+        void CloseWithAnim() {
+            canvasRaycaster.enabled = false;
+            outSeq.PlaySequence();
+            outSeq.sequence.onComplete += Close;
         }
 
         void setupLevelViews() {
